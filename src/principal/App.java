52d687193462863b4369eb60928889894ba1ca0b
camilla-sr <camilla.sreis2@gmail.com>
2024-10-20 03:10:38 -0300

    feat: criadas instâncias das classes na main para facilitar os testes

--- src/principal/App.java
+++ src/principal/App.java
@@ -1,21 +1,33 @@
 package principal;
 
 import java.util.Scanner;
+import dao.AgendamentoDAO;
+import dao.ClienteDAO;
+import dao.EstoqueDAO;
+import dao.FuncionarioDAO;
+import dao.LoteDAO;
+import dao.PecaDAO;
+import dao.TipoServicoDAO;
 
 public class App {
 
+    //Use essa main para fazer os testes dos métodos
     public static void main(String[] args) {
+        //uso do método scanner para realizar os testes
+        //adicionei um utf8 para eliminar os erros de caracteres quebrados
         Scanner sc = new Scanner(System.in, "utf8");
         Scanner num = new Scanner(System.in);
-        // em primeiro momento, main criada para testes com banco
-        // TESTES OK
-        //Tipo serviço
-        //Peça
-        //Funcionário
-        //Funcionamento
-        //Estoque
+        
+        //todas as classes estão instanciadas aqui para facilitar os testes
+        AgendamentoDAO  ag = new AgendamentoDAO();
+        ClienteDAO      cl = new ClienteDAO();
+        EstoqueDAO      e = new EstoqueDAO();
+        FuncionarioDAO  f = new FuncionarioDAO();
+        LoteDAO         lt = new LoteDAO();
+        PecaDAO         pc = new PecaDAO();
+        TipoServicoDAO  ts = new TipoServicoDAO();
 
-
+        //chame os métodos para testes no espaço abaixo
         
     }
 }
