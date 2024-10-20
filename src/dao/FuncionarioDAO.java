433997475cf7bb8c8d65fb700a90bb6dbb41e046
camilla-sr <camilla.sreis2@gmail.com>
2024-10-20 00:46:16 -0300

    feat: finalizado o CRUD para classe de Funcionario e Peca

--- src/dao/FuncionarioDAO.java
+++ src/dao/FuncionarioDAO.java
@@ -5,7 +5,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class FuncionarioDAO {
-
     final Conexao conn = new Conexao();
     private int idFuncionario;
     private String nomeFuncionario;
@@ -24,7 +23,6 @@ public class FuncionarioDAO {
 // Método para listar todos os funcionários do banco de dados
     public void listarFuncionarios() {
         String sqlConsulta = "SELECT * FROM funcionario order by nome_funcionario";
-
         try {
             ResultSet lista = conn.executarConsulta(sqlConsulta);
             System.out.println("\nFuncionários");
@@ -44,55 +42,34 @@ public class FuncionarioDAO {
     }
 
     public void editarFuncionario(int idFuncionario, String novoNome) {
-        try {
-            String sql = "SELECT * FROM funcionario where id_funcionario = " + idFuncionario;
-            ResultSet retorno = conn.executarConsulta(sql);
-
-            int row = 0;
-            if (retorno != null && retorno.next()) {
-                row = retorno.getInt("id_funcionario");
-                int funcionarioValido = validaID(row);
-                if (funcionarioValido == 2) {
-                    System.out.println("Funcionário não encontrado na base");
-                } else {
-                    String sqlEdit = "UPDATE funcionario set nome_funcionario = '"+novoNome+"' where id_funcionario = "+idFuncionario+"";
-                    boolean resposta = conn.executar(sqlEdit);
-                    if (resposta == true) {
-                        System.out.println("Funcionário editado");
-                    } else {
-                        System.out.println("Algo deu errado");
-                    }
-                }
+        int funcionarioValido = validaID(idFuncionario);
+        if (funcionarioValido == 2) {
+            System.out.println("Funcionário não encontrado na base");
+        } else {
+            String sqlEdit = "UPDATE funcionario set nome_funcionario = '" + novoNome + "' where id_funcionario = " + idFuncionario + "";
+            boolean resposta = conn.executar(sqlEdit);
+            if (resposta == true) {
+                System.out.println("Funcionário editado");
+            } else {
+                System.out.println("Algo deu errado");
             }
-        } catch (SQLException e) {
-            System.out.println("Algo deu errado" + e.getMessage());
         }
     }
-    
-    public void apagarFuncionario(int idFuncionario){
-        try {
-            String sql = "SELECT * FROM funcionario where id_funcionario = " + idFuncionario;
-            ResultSet retorno = conn.executarConsulta(sql);
 
-            int row = 0;
-            if (retorno != null && retorno.next()) {
-                row = retorno.getInt("id_funcionario");
-                int funcionarioValido = validaID(row);
-                if (funcionarioValido == 2) {
-                    System.out.println("Funcionário não encontrado na base");
-                } else {
-                    String sqlDel = "DELETE from funcionario where id_funcionario = " + idFuncionario;
-                    boolean resposta = conn.executar(sqlDel);
-                    if (resposta == true) {
-                        System.out.println("Funcionário deletado");
-                    } else {
-                        System.out.println("Algo deu errado");
-                    }
-                }
+    public void apagarFuncionario(int idFuncionario) {
+        int funcionarioValido = validaID(idFuncionario);
+        if (funcionarioValido == 2) {
+            System.out.println("Funcionário não encontrado na base");
+        } else {
+            String sqlDel = "DELETE from funcionario where id_funcionario = " + idFuncionario;
+            boolean resposta = conn.executar(sqlDel);
+            if (resposta == true) {
+                System.out.println("Funcionário deletado");
+            } else {
+                System.out.println("Algo deu errado");
             }
-        } catch (SQLException e) {
-            System.out.println("Algo deu errado" + e.getMessage());
         }
+        conn.desconectar();
     }
 
 // -------------- MÉTODOS DE APOIO --------------    
@@ -108,6 +85,8 @@ public class FuncionarioDAO {
             }
         } catch (SQLException e) {
             System.out.println("Algo deu errado" + e.getMessage());
+        } finally {
+            conn.desconectar();
         }
         return resposta;
     }
@@ -116,18 +95,14 @@ public class FuncionarioDAO {
     public int getIdFuncionario() {
         return idFuncionario;
     }
-
     public void setIdFuncionario(int idFuncionario) {
         this.idFuncionario = idFuncionario;
     }
-
     public String getNomeFuncionario() {
         return nomeFuncionario;
     }
-
     public void setNomeFuncionario(String nomeFuncionario) {
         this.nomeFuncionario = nomeFuncionario;
     }
-
 // ------------------------------------------------
 }
--- src/dao/PecaDAO.java
+++ src/dao/PecaDAO.java
@@ -9,74 +9,97 @@ public class PecaDAO {
     private int idPeca;
     private String descricaoPeca;
     private int qntdPeca;
-    
+
     // Métodos Principais
-    public void cadastrarPeca(String descricaoPeca, int qntdPeca){
-        
-        String sqlInserir = "INSERT into peca (tipo_produto, desc_peca, qntd_peca)"
-                + "VALUES ('"+descricaoPeca+"', "+qntdPeca+")";
-        
+    public void cadastrarPeca(String descricaoPeca, int qntdPeca) {
+        String sqlInserir = "INSERT into peca (desc_peca, qntd_peca)"
+                + "VALUES ('" + descricaoPeca + "', " + qntdPeca + ")";
+
         boolean resposta = conn.executar(sqlInserir);
-        if(resposta == true){
+        if (resposta == true) {
             System.out.println("Peça inserida");
-        }else{
+        } else {
             System.out.println("Algo deu errado");
         }
+        conn.desconectar();
     }
-    
-    public void editarPeca(int idPeca, String novaDescricao, int novaQntd){
-        String sqlEdit = "UPDATE peca set desc_peca = '"+novaDescricao+"', qntd_peca = "+novaQntd+" where id_peca = "+idPeca+"";
-        
-        boolean resposta = conn.executar(sqlEdit);
-        if(resposta == true){
-            System.out.println("Peça Editada");
-        }else{
-            System.out.println("Algo deu errado");
+
+    public void editarPeca(int idPeca, String novaDescricao, int novaQntd) {
+        int pecaValida = validaID(idPeca);
+
+        if (pecaValida == 2) {
+            System.out.println("Peça não encontrada na base");
+        } else {
+            String sqlEdit = "UPDATE peca set desc_peca = '" + novaDescricao
+                    + "', qntd_peca = " + novaQntd + " where id_peca = " + idPeca + "";
+
+            boolean resposta = conn.executar(sqlEdit);
+            if (resposta == true) {
+                System.out.println("Peça Editada");
+            } else {
+                System.out.println("Algo deu errado");
+            }
         }
+        conn.desconectar();
     }
-    
-    public void listarPecas(){
+
+    public void listarPecas() {
         String sqlConsulta = "SELECT * from peca";
+        System.out.println("---------------------------");
         ResultSet lista = conn.executarConsulta(sqlConsulta);
-        
+
         try {
             while (lista.next()) {
                 int id = lista.getInt("id_peca");
-                String tipoProduto = lista.getString("tipo_produto");
                 String descricaoPeca = lista.getString("desc_peca");
                 int quantidade = lista.getInt("qntd_peca");
 
                 System.out.println("ID: " + id);
-                System.out.println("Tipo do Produto: " + tipoProduto);
                 System.out.println("Peça: " + descricaoPeca);
                 System.out.println("Quantidade: " + quantidade);
                 System.out.println("---------------------------");
             }
-            
             lista.close();
         } catch (SQLException e) {
             System.out.println("Erro ao processar resultado: " + e.getMessage());
         } finally {
             conn.desconectar();
         }
-            
+    }
+    
+    public void apagarPeca(int idPeca) {
+        int pecaValida = validaID(idPeca);
+        if (pecaValida == 2) {
+            System.out.println("Peça não encontrada na base");
+        } else {
+            String sqlDel = "DELETE from peca where id_peca = " + idPeca;
+            boolean resposta = conn.executar(sqlDel);
+            if (resposta == true) {
+                System.out.println("Peça deletada");
+            } else {
+                System.out.println("Algo deu errado");
+            }
+        }
+        conn.desconectar();
     }
 
 // -------------- MÉTODOS DE APOIO- --------------    
-    // !!!!!
-    public void validaID(int id){
-        try{
-            String sql = "SELECT id_peca from peca where id_peca = "+id+"";
+    private int validaID(int id) {
+        int resposta = 0;
+        try {
+            String sql = "SELECT * from peca where id_peca = " + id;
             ResultSet retorno = conn.executarConsulta(sql);
-
-            int row = 0;
             if (retorno != null && retorno.next()) {
-                row = retorno.getInt("id_peca");
-                System.out.println(row);
+                resposta = 1;
+            } else {
+                resposta = 2;
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println("Algo deu errado" + e.getMessage());
+        } finally {
+            conn.desconectar();
         }
+        return resposta;
     }
 
 // -------------- GETTERS E SETTERS --------------
