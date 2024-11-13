11875b98ec80c0f45abd840e0cbf05e5dc4e19ea
ENRICO CARBONARI KHARMANDAIAN <enrico.ckharmandaian@senacsp.edu.br>
2024-11-13 19:47:56 -0300

    pfv vai

--- src/principal/Estoque.java
+++ src/principal/Estoque.java
@@ -1,6 +1,71 @@
 package principal;
 
 
+import dao.EstoqueDAO;
+import dao.FuncionarioDAO;
+import include.Helper;
+import java.util.Scanner;
+
+
 public class Estoque {
-    // classe que vai controlar entrada de dados;
+    
+    Integer numeroValidado = null;
+    Helper h = new Helper();
+    Scanner sc = new Scanner(System.in, "utf8");
+    Scanner num = new Scanner(System.in);
+    FuncionarioDAO fn = new FuncionarioDAO();
+    
+    private int idEstoque;
+    private int qntdEstoque;
+    private String dataUltimaAtualizacao;
+    
+    public void estoque (){
+        boolean cad = false;
+        
+        System.out.print("Quantidade no estoque: ");
+        
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // -------------- GETTERS E SETTERS --------------
+    public int getIdEstoque() {
+        return idEstoque;
+    }
+
+    public void setIdEstoque(int idEstoque) {
+        this.idEstoque = idEstoque;
+    }
+
+    public int getQntdEstoque() {
+        return qntdEstoque;
+    }
+
+    public void setQntdEstoque(int qntdEstoque) {
+        this.qntdEstoque = qntdEstoque;
+    }
+
+    public String getDataUltimaAtualizacao() {
+        return dataUltimaAtualizacao;
+    }
+
+    public void setDataUltimaAtualizacao(String dataUltimaAtualizacao) {
+        this.dataUltimaAtualizacao = dataUltimaAtualizacao;
+    }
+    // -----------------------------------------------
 }
--- src/principal/Funcionario.java
+++ src/principal/Funcionario.java
@@ -1,6 +1,50 @@
 package principal;
 
-public class Funcionario {
-    // classe que vai controlar entrada de dados;
+import dao.FuncionarioDAO;
+import include.Helper;
+import java.util.Scanner;
 
+public class Funcionario {
+
+    Integer numeroValidado = null;
+    Helper h = new Helper();
+    Scanner sc = new Scanner(System.in, "utf8");
+    Scanner num = new Scanner(System.in);
+    FuncionarioDAO fn = new FuncionarioDAO();
+
+    private int idFuncionario;
+    private String nomeFuncionario;
+
+    public void addFuncionario(){
+        boolean cad = false;
+        
+        System.out.print("Nome do funcionário: ");
+        setNomeFuncionario(sc.nextLine());
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    // -------------- GETTERS E SETTERS --------------
+    public int getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(int idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+    
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+    //----------------------------------------------------
 }
