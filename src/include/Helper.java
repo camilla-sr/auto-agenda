81bd24c436b2e1f8725fffd5371458a272fced69
camilla-sr <camilla.sreis2@gmail.com>
2024-12-01 20:51:55 -0300

    feat: funções de validação de datas estão mais robustas

--- src/include/Helper.java
+++ src/include/Helper.java
@@ -1,5 +1,9 @@
 package include;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Helper {
     /*função para validar todas as entradas numéricas,
     para impedir que o sistema quebre caso a entrada não seja
@@ -12,23 +16,33 @@ public class Helper {
         }
     }
 
+    // Novos padrões de código para o formato das datas
     public String dataPadraoBanco(String data) {
-        String[] dataPartes = data.split("/"); //separo o dado obtido no input
-        if (dataPartes.length == 3) {
-            return dataPartes[2] + "-" + dataPartes[1] + "-" + dataPartes[0]; // yyyy/MM/dd
-        } else {
-            System.out.println("Formato de data inválido");
-            return null;
+        try {            
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); // aqui recebo a data no padrão informado pelo usuário      
+            sdf.setLenient(false); // Não permite datas doidas como 32/13/2024
+            Date formatoBanco = sdf.parse(data); //salva a data formatada numa variável do tipo data
+
+            // Retorna a data formatada no padrão yyyy-MM-dd
+            SimpleDateFormat formatoBancoSaida = new SimpleDateFormat("yyyy-MM-dd");
+            return formatoBancoSaida.format(formatoBanco);
+        } catch (ParseException e) {            
+            return null; // Se a data for inválida, retorna null
         }
     }
 
     public String dataPadraoBR(String data) {
-        String[] dataPartes = data.split("-"); //separo o dado obtido no input
-        if (dataPartes.length == 3) {
-            return dataPartes[2] + "/" + dataPartes[1] + "/" + dataPartes[0];
-        } else {
-            System.out.println("Formato de data inválido");
-            return null;
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); //recebo a data no padrão do banco
+            sdf.setLenient(false); // Não permite datas absurdas
+            Date formatoBR = sdf.parse(data); //adiciona a data para a variável válida do tipo data
+
+            // Converte para o formato dd/MM/yyyy
+            SimpleDateFormat formatoBRSaída = new SimpleDateFormat("dd/MM/yyyy");
+            return formatoBRSaída.format(formatoBR);
+        } catch (ParseException e) {
+            return null; // Se a data for inválida, retorna null
         }
     }
+
 }
