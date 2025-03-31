a996b09cf7aa802fa49907a70ffbe2cc5e14e037
Camilla S. Reis <camilla.sreis2@gmail.com>
2025-03-31 00:27:05 -0300

    add: mudei as coisas que o Hector apontou no grupo

--- Versão 1.5/assets/script.js
+++ Versão 1.5/assets/script.js
@@ -3,3 +3,14 @@ fetch('../assets/barra.html')
     .then(data => {
         document.getElementById('barra').innerHTML = data;
     });
+
+
+    //esse carai aqui não tá funfando ainda
+const pageAtual = window.location.pathname.split('/').pop(); // "index.html"
+    
+document.querySelectorAll('.nav-link').forEach(link => {
+    if (link.getAttribute('href') === pageAtual) {
+        console.log(link)
+        link.parentElement.classList.add('active');
+    }
+});
