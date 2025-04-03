d3ac117725513d513e0aec9b8cef29744923b536
CAMILLA DE SOUZA REIS <camilla.sreis@senacsp.edu.br>
2025-04-02 21:58:28 -0300

    add: pasta do spring boot nessa bomba

---
+++ Versão Spring/autoagenda/src/main/java/br/com/autoagenda/autoagenda/AutoagendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.autoagenda.autoagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoagendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoagendaApplication.class, args);
+	}
+
+}
