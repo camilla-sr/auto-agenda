b479a844fb6e8f08736b91e3ad9f1911185009ee
Dymitri <dymitrif@vivaldi.net>
2025-11-22 11:23:18 -0300

    style: fix visuais da tabela de agendamentos

--- vSpring/autoagenda/src/main/java/br/com/autoagenda/autoagenda/controller/C_Agendamento.java
+++ vSpring/autoagenda/src/main/java/br/com/autoagenda/autoagenda/controller/C_Agendamento.java
@@ -1,13 +1,17 @@
 package br.com.autoagenda.autoagenda.controller;
 
 import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
 import br.com.autoagenda.autoagenda.model.Agendamento;
 import br.com.autoagenda.autoagenda.model.Servico;
 import br.com.autoagenda.autoagenda.repositorios.AgendamentoRepository;
@@ -77,4 +81,16 @@ public class C_Agendamento {
 		if(idAgendamento != null) { repo.deleteById(idAgendamento); }
 		return "redirect:/agendamentos?apagar=true";
 	}
+
+	@PostMapping("/concluir/{id}")
+    @ResponseBody
+    public Agendamento concluirAgendamento(@PathVariable("id") Integer id) {
+        Agendamento agendamento = repo.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Agendamento não encontrado"));
+        
+        agendamento.setStatusAgendamento("Concluido");
+        agendamento.setDataConclusao(LocalDate.now());
+        
+        return repo.save(agendamento);
+    }
 }
